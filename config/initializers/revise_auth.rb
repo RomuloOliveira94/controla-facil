498abc5a589aab5bf314e6f2c8a485da1a6bc429
Rômulo Augusto <romuloffall@gmail.com>
2024-07-03 01:01:29 -0300

    #add name and last name on sign up ao edit form

--- config/initializers/revise_auth.rb
+++ config/initializers/revise_auth.rb
@@ -2,4 +2,7 @@ ReviseAuth.configure do |config|
   # Customize the params for registration and update profile.
   # config.sign_up_params += [:time_zone]
   # config.update_params += [:time_zone]
+  config.minimum_password_length = 6
+  config.sign_up_params += %i[first_name last_name avatar]
+  config.update_params += %i[first_name last_name avatar]
 end
