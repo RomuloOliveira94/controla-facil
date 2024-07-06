bc7a15e7e912b2cccc624b0c22a0a39d6422ae5b
Rômulo Augusto <romuloffall@gmail.com>
2024-07-06 14:37:57 -0300

    #implement table to see incomes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def format_brl_currency(value)
     number_to_currency(value, unit: "R$ ", separator: ",", delimiter: ".", precision: 2)
   end
+
+  def format_boolean(value)
+    value ? "Sim" : "Não"
+  end
 end
