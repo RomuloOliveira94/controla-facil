f246ae79d91b0dd0c6d6a6bddbbf16bf464a4731
Rômulo Augusto <romuloffall@gmail.com>
2024-07-06 23:38:22 -0300

    #fix responsiveness on menu

--- app/javascript/controllers/nav_bar_controller.js
+++ app/javascript/controllers/nav_bar_controller.js
@@ -1,13 +1,16 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="nav-bar"
 export default class extends Controller {
-  static targets = [ "toggle", "btn" ]
+  static targets = ["toggle", "btn"];
 
   connect() {
+    if (window.innerWidth < 768) {
+      this.toggleTarget.classList.add("hidden");
+    }
   }
 
   toggle() {
-    this.toggleTarget.classList.toggle('hidden');
+    this.toggleTarget.classList.toggle("hidden");
   }
 }
