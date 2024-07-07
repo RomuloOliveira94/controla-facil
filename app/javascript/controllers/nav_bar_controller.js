25622ef37afcafd3ccc5cb48ea1ac942bccd4614
Rômulo Augusto <romuloffall@gmail.com>
2024-07-06 23:58:41 -0300

    #add activation on link

--- app/javascript/controllers/nav_bar_controller.js
+++ app/javascript/controllers/nav_bar_controller.js
@@ -2,15 +2,27 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="nav-bar"
 export default class extends Controller {
-  static targets = ["toggle", "btn"];
+  static targets = ["toggle", "btn", "links"];
 
   connect() {
     if (window.innerWidth < 768) {
       this.toggleTarget.classList.add("hidden");
     }
+
+    this.activateLink();
   }
 
   toggle() {
     this.toggleTarget.classList.toggle("hidden");
+  }
+
+  activateLink() {
+    this.linksTargets.forEach((link) => {
+      if (link.href === window.location.href) {
+        link.classList = "text-primary border-b-2 border-primary pb-[18px]";
+      } else {
+        link.classList = "text-gray-500";
+      }
+    });
   }
 }
