e3cca72c889ee5fc71299ee5f3460d75466b3850
Rômulo Augusto <romuloffall@gmail.com>
2024-07-10 01:09:31 -0300

    #implement remove categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,8 @@
 class CategoriesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_frame_response, only: %i[ create ]
+  before_action :set_category, only: %i[ destroy ]
+
   def new
     @category = Category.new
   end
@@ -8,8 +12,8 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.turbo_stream do
-          render turbo_stream: turbo_stream.append('categories', partial: 'configurations/categories_list',
+          format.turbo_stream do
+          render turbo_stream: turbo_stream.append(list_to_update, partial: 'configurations/categories_list',
                                                                  locals: { category: @category })
         end
         format.html { redirect_to configurations_path, notice: 'Categoria criada com sucesso.' }
@@ -20,13 +24,31 @@ class CategoriesController < ApplicationController
   end
 
   def destroy
-    @category = Category.find(params[:id])
-    @category.destroy
+    @category.destroy!
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.remove(@category)
+      end
+      format.html { redirect_to configurations_path, notice: 'Categoria deletada com sucesso.' }
+    end
   end
 
   private
 
+  def ensure_frame_response
+    redirect_to configurations_path unless turbo_frame_request?
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
   def category_params
     params.require(:category).permit(:name, :description, :cat_sub)
+  end
+
+  def list_to_update
+    @category.cat_sub === "expenses" ? "expenses" : "incomes"
   end
 end
