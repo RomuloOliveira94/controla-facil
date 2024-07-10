7b2d16ec48f18f24e6e9f4e74adfff23cb61aa50
Rômulo Augusto <romuloffall@gmail.com>
2024-07-10 11:07:32 -0300

    #disassociate when delete categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,4 +5,14 @@ class Category < ApplicationRecord
 
   validates :name, presence: true
   validates :cat_sub, presence: true
+
+  before_destroy :disassociate_uses
+
+  def disassociate_uses
+    if self.cat_sub === "expenses"
+      self.expenses.update_all(category_id: Category.find_by(name: 'Other').id)
+    else
+      self.incomes.update_all(category_id: Category.find_by(name: 'Other').id)
+    end
+  end
 end
