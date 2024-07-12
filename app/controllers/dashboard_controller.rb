66a882f4b2c5066f8c1a877019a60100b4dea0b2
Rômulo Augusto <romuloffall@gmail.com>
2024-07-11 23:32:40 -0300

    #add list to show resume on dashboard page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,8 @@ class DashboardController < ApplicationController
   before_action :total_incomes
   before_action :total_expenses
   before_action :total_balance
-  
+  before_action :all_incomes_and_expenses
+
   include BalanceHelper
 
   def index; end
@@ -20,5 +21,11 @@ class DashboardController < ApplicationController
 
   def total_balance
     @total_balance ||= total_incomes - total_expenses
+  end
+
+  def all_incomes_and_expenses
+    @all_incomes_and_expenses ||= user_actual_month_yeah_balance.incomes + user_actual_month_yeah_balance.expenses
+
+    @all_incomes_and_expenses.sort_by!(&:created_at).reverse!
   end
 end
