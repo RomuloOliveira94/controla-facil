c70f4eafbf3170f056c7f843754a9553d6f3cf7d
Rômulo Augusto <romuloffall@gmail.com>
2024-07-11 23:19:15 -0300

    refactor on balance concern

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,24 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :total_incomes
+  before_action :total_expenses
+  before_action :total_balance
+  
   include BalanceHelper
 
-  def index
-    @balance = @user_actual_month_yeah_balance
-    @total_incomes = total_incomes
-    @total_expenses = total_expenses
-    @total_balance = total_balance
+  def index; end
+
+  private
+
+  def total_incomes
+    @total_incomes ||= user_actual_month_yeah_balance.incomes.sum(:value)
+  end
+
+  def total_expenses
+    @total_expenses ||= user_actual_month_yeah_balance.expenses.sum(:value)
+  end
+
+  def total_balance
+    @total_balance ||= total_incomes - total_expenses
   end
 end
