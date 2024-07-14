1087eada7209c88d571137faa497f21ba9ba9153
Rômulo Augusto <romuloffall@gmail.com>
2024-07-14 14:17:58 -0300

    #implement category limit

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,16 +8,26 @@ class Category < ApplicationRecord
   validates :icon, presence: true
 
   before_destroy :disassociate_uses
+  before_create :check_user_category_limit
 
-  scope :user_global, ->(user) { where("user_id IS NULL OR user_id = ?", user.id) }
+  scope :user_global, ->(user) { where('user_id IS NULL OR user_id = ?', user.id) }
   scope :incomes, -> { where(cat_sub: 'incomes').order(:created_at) }
   scope :expenses, -> { where(cat_sub: 'expenses').order(:created_at) }
 
   def disassociate_uses
-    if self.cat_sub === "expenses"
-      self.expenses.update_all(category_id: Category.find_by(name: 'Other').id)
+    if cat_sub === 'expenses'
+      expenses.update_all(category_id: Category.find_by(name: 'Other').id)
     else
-      self.incomes.update_all(category_id: Category.find_by(name: 'Other').id)
+      incomes.update_all(category_id: Category.find_by(name: 'Other').id)
     end
+  end
+
+  def check_user_category_limit
+    puts "Checking user category limit for user #{user.id}"
+    puts "User has #{user.categories.where(cat_sub:).count} categories"
+    return unless user.categories.where(cat_sub:).count >= 10
+
+    errors.add(:base, 'Você atingiu o limite de categorias, exclua alguma para adicionar novas.')
+    throw :abort
   end
 end
