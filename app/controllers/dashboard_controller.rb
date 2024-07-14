a8f203c4c4719000420d4cb2d92111766903cf47
Rômulo Augusto <romuloffall@gmail.com>
2024-07-14 13:18:41 -0300

    #implement conditions to show values

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,19 +12,30 @@ class DashboardController < ApplicationController
   private
 
   def total_incomes
-    @total_incomes ||= user_actual_month_yeah_balance.incomes.sum(:value)
+    @total_incomes ||= user_actual_month_yeah_balance.try(:incomes)&.sum(:value)
   end
 
   def total_expenses
-    @total_expenses ||= user_actual_month_yeah_balance.expenses.sum(:value)
+    @total_expenses ||= user_actual_month_yeah_balance.try(:expenses)&.sum(:value)
   end
 
   def total_balance
+    if total_incomes.nil? || total_expenses.nil?
+      @total_balance = 0
+      return
+    end
+
     @total_balance ||= total_incomes - total_expenses
   end
 
   def all_incomes_and_expenses
-    @all_incomes_and_expenses ||= user_actual_month_yeah_balance.incomes + user_actual_month_yeah_balance.expenses
+    if user_actual_month_yeah_balance.nil?
+      @all_incomes_and_expenses = []
+      return
+    end
+
+
+    @all_incomes_and_expenses ||= user_actual_month_yeah_balance.try(:incomes) + user_actual_month_yeah_balance.try(:expenses)
 
     @all_incomes_and_expenses.sort_by!(&:created_at).reverse!
   end
