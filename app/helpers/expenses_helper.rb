0d310ed600b77fd34c02ad458b16e28a983f521b
Rômulo Augusto <romuloffall@gmail.com>
2024-07-15 00:29:00 -0300

    #implement helpers for the submit buttons

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,5 @@
 module ExpensesHelper
+  def submit_expense_button_text
+    @expense.new_record? ? "Gerar novo custo" : "Atualizar custo"
+  end
 end
--- app/helpers/incomes_helper.rb
+++ app/helpers/incomes_helper.rb
@@ -1,2 +1,6 @@
 module IncomesHelper
+
+  def submit_income_button_text
+    @income.new_record? ? "Gerar nova receita" : "Atualizar receita"
+  end
 end
