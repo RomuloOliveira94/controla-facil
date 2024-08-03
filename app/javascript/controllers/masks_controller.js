3e9e687cf96a56c3c28ec61242091398536d39c9
Rômulo Augusto <romuloffall@gmail.com>
2024-08-03 02:17:04 -0300

    #add mask to search values

--- app/javascript/controllers/masks_controller.js
+++ app/javascript/controllers/masks_controller.js
@@ -3,11 +3,31 @@ import { MaskInput } from "maska";
 
 // Connects to data-controller="masks"
 export default class extends Controller {
-  static targets = ["brl"];
+  static targets = ["money", "brl"];
 
   connect() {
+    if (this.hasBrlTarget) {
+      this.loadBrl();
+    }
+
+    if (this.hasMoneyTarget) {
+      this.loadMoney();
+    }
+  }
+
+  loadBrl() {
     new MaskInput(this.brlTarget, {
       mask: "R$ 0,99",
+      tokens: {
+        0: { pattern: /[0-9]/, multiple: true },
+        9: { pattern: /[0-9]/ },
+      },
+    });
+  }
+
+  loadMoney() {
+    new MaskInput(this.moneyTarget, {
+      mask: "0,99",
       tokens: {
         0: { pattern: /[0-9]/, multiple: true },
         9: { pattern: /[0-9]/ },
