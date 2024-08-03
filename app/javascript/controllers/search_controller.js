50f16e9a34c40365385a7771e1af9175235f5bf0
Rômulo Augusto <romuloffall@gmail.com>
2024-08-03 02:59:31 -0300

    #fix responsiveness and add button to toggle search options

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,10 +2,16 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  static targets = ["form"];
+  static targets = ["form", "searchToggle"];
 
   connect() {
     this.timeout = null;
+  }
+
+  toggleForm() {
+    this.formTarget.classList.toggle("hidden");
+    this.searchToggleTarget.classList.toggle("btn-primary");
+    this.searchToggleTarget.classList.toggle("btn-default");
   }
 
   search() {
