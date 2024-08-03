a0b9b80f1889b1b06d32c2e91c8e2c4f347868dd
Rômulo Augusto <romuloffall@gmail.com>
2024-08-03 04:08:14 -0300

    #implement buttons to show search forms

--- app/javascript/controllers/search_controller.js
+++ app/javascript/controllers/search_controller.js
@@ -2,16 +2,66 @@ import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="search"
 export default class extends Controller {
-  static targets = ["form", "searchToggle"];
+  static targets = [
+    "form",
+    "datas",
+    "description",
+    "value",
+    "select",
+    "formButton",
+    "clearButton",
+  ];
+  static values = {
+    showDatas: Boolean,
+    showText: Boolean,
+    showSelect: Boolean,
+  };
 
   connect() {
     this.timeout = null;
+    this.showDatasValue = false;
+    this.showTextValue = false;
+    this.showSelectValue = false;
   }
 
-  toggleForm() {
-    this.formTarget.classList.toggle("hidden");
-    this.searchToggleTarget.classList.toggle("btn-primary");
-    this.searchToggleTarget.classList.toggle("btn-default");
+  toggleDatas() {
+    this.showDatasValue = !this.showDatasValue;
+
+    if (this.showDatasValue) {
+      this.show(this.datasTarget);
+      this.show(this.formButtonTarget);
+      this.show(this.clearButtonTarget);
+    } else {
+      this.hide(this.datasTarget);
+      this.hide(this.formButtonTarget);
+      this.hide(this.clearButtonTarget);
+    }
+  }
+
+  toggleText() {
+    this.showTextValue = !this.showTextValue;
+
+    if (this.showTextValue) {
+      this.show(this.descriptionTarget);
+      this.show(this.valueTarget);
+      this.show(this.clearButtonTarget);
+    } else {
+      this.hide(this.descriptionTarget);
+      this.hide(this.valueTarget);
+      this.hide(this.clearButtonTarget);
+    }
+  }
+
+  toggleSelect() {
+    this.showSelectValue = !this.showSelectValue;
+
+    if (this.showSelectValue) {
+      this.show(this.selectTarget);
+      this.show(this.clearButtonTarget);
+    } else {
+      this.hide(this.selectTarget);
+      this.hide(this.clearButtonTarget);
+    }
   }
 
   search() {
@@ -19,5 +69,15 @@ export default class extends Controller {
     this.timeout = setTimeout(() => {
       this.formTarget.requestSubmit();
     }, 500);
+  }
+
+  show(element) {
+    element.classList.remove("hidden");
+    element.classList.add("flex");
+  }
+
+  hide(element) {
+    element.classList.add("hidden");
+    element.classList.remove("flex");
   }
 }
