c2db83155cf7287c3e88d058fab77e314fce778a
Rômulo Augusto <romuloffall@gmail.com>
2024-08-05 01:01:28 -0300

    #add validation to create only on actual balance

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -7,11 +7,16 @@ class Expense < ApplicationRecord
   validates :balance_id, presence: true
   validates :user_id, presence: true
   validates :date, presence: true
+  validate :date_only_on_current_month, on: [:create, :update]
 
   include UpdateBalanceValue
 
   def self.ransackable_attributes(_auth_object = nil)
     %w[balance_id category_id created_at day description fixed id updated_at
        user_id value]
+  end
+
+  def date_only_on_current_month
+    errors.add(:date, 'deve ser do mês corrente') unless date.month == Time.zone.today.month
   end
 end
