f801002d7e04873a1d9f9ba6494d47df469577dc
Rômulo Augusto <romuloffall@gmail.com>
2024-09-01 22:16:30 -0300

    #fix categorie creation and transale data

--- app/models/category.rb
+++ app/models/category.rb
@@ -10,12 +10,12 @@ class Category < ApplicationRecord
   before_destroy :disassociate_uses
   before_create :check_user_category_limit
 
-  scope :user_global, ->(user) { where('user_id IS NULL OR user_id = ?', user.id) }
+  scope :user_global, ->(user) { where('fixed IS TRUE OR user_id = ?', user.id) }
   scope :incomes, -> { where(cat_sub: 'incomes').order(:created_at) }
   scope :expenses, -> { where(cat_sub: 'expenses').order(:created_at) }
 
   def disassociate_uses
-    if cat_sub === 'expenses'
+    if cat_sub == 'expenses'
       expenses.update_all(category_id: Category.find_by(name: 'Other').id)
     else
       incomes.update_all(category_id: Category.find_by(name: 'Other').id)
@@ -23,7 +23,7 @@ class Category < ApplicationRecord
   end
 
   def check_user_category_limit
-    return unless user.categories.where(cat_sub:).count >= 10
+    return unless user.categories.where(cat_sub:).count >= 15
 
     errors.add(:base, 'Você atingiu o limite de categorias, exclua alguma para adicionar novas.')
     throw :abort
