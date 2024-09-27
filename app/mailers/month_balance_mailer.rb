8ab772a3daf6e99e798ec4a6cd58b5d8ec0c818c
Rômulo Augusto <romuloffall@gmail.com>
2024-09-27 15:03:35 -0300

    #formating the balance details

--- app/mailers/month_balance_mailer.rb
+++ app/mailers/month_balance_mailer.rb
@@ -5,6 +5,7 @@ class MonthBalanceMailer < ApplicationMailer
   #   en.month_balance_mailer.month_balance_email.subject
   #
   def month_balance_email
+    @greeting = 'Olá'
     @user = params[:user]
     @balance = params[:balance]
     @month = I18n.t('date.month_array')[Date.today.prev_month.month]
@@ -12,6 +13,23 @@ class MonthBalanceMailer < ApplicationMailer
     @total_incomes = format_brl_currency(params[:total_incomes] || 0)
     @total_expenses = format_brl_currency(params[:total_expenses] || 0)
     @total_balance = format_brl_currency(@balance.balance || 0)
+
+    @formatted_incomes = @balance.incomes.collect do |income|
+      {
+        value: format_brl_currency(income.value),
+        date: income.created_at.strftime('%d/%m/%Y'),
+        description: income.description
+      }
+    end
+
+    @formatted_expenses = @balance.expenses.collect do |expense|
+      {
+        value: format_brl_currency(expense.value),
+        date: expense.created_at.strftime('%d/%m/%Y'),
+        description: expense.description
+      }
+    end
+
     mail to: @user.email, subject: "Balanço financeiro do mês de #{@month}/#{@year} - Controla Fácil"
   end
 
