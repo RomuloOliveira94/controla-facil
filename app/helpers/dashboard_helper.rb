297ef6e29c42b3b959eda85f6ac55016bed37104
Rômulo Augusto <romuloffall@gmail.com>
2024-10-11 23:15:48 -0300

    #improve ui feedback

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,9 @@
 module DashboardHelper
+  def positive_check(number)
+    number.positive? ? 'from-green-400 to-green-600' : 'from-red-400 to-red-600'
+  end
+
+  def positive_bool(number)
+    number.positive? ? true : false
+  end
 end
