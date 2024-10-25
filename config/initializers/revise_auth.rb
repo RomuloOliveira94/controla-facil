85d9549a0f75b6dbf6a2213f1359f6c68f57db72
Rômulo Oliveira <romuloffall@gmail.com>
2024-10-25 11:31:52 -0300

    #fix password length

--- config/initializers/revise_auth.rb
+++ config/initializers/revise_auth.rb
@@ -4,4 +4,5 @@ ReviseAuth.configure do |config|
   # config.update_params += [:time_zone]
   config.sign_up_params += %i[first_name last_name avatar provider_token provider password_digest]
   config.update_params += %i[first_name last_name avatar]
+  config.minimum_password_length = 8
 end
