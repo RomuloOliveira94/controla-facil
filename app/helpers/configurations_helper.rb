7e5310cb173b8c8b43becdfe34d63a030b2c9015
Rômulo Oliveira <romuloffall@gmail.com>
2025-03-03 19:36:19 -0300

    #remove unnecessary themes

--- app/helpers/configurations_helper.rb
+++ app/helpers/configurations_helper.rb
@@ -1,27 +1,7 @@
 module ConfigurationsHelper
-  THEMES = %w[light
-              dark
-              cupcake
-              bumblebee
-              emerald
-              corporate
-              synthwave
-              retro
-              halloween
-              garden
-              lofi
-              pastel
-              fantasy
-              wireframe
-              dracula
-              cmyk
-              autumn
-              business
-              acid
-              lemonade
-              night
-              winter
-              dim
-              nord
-              sunset].freeze
+  THEMES = %w[
+    light
+    dark
+    night
+  ].freeze
 end
